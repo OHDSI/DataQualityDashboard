8cf57f923a82cb4f5eece97ac89bcad373e56f5f
Ajit Londhe <alondhe2@its.jnj.com>
2019-08-23 11:34:13 -0700

    Added missing dbType in test

--- tests/testthat/test-execute.R
+++ tests/testthat/test-execute.R
@@ -1,34 +1,41 @@
 library(testthat)
 
 test_that("Execute DQ checks", {
-  sysUser <- Sys.getenv(sprintf("CDM5_%s_USER", toupper(dbType)))
-  sysPassword <- URLdecode(Sys.getenv(sprintf("CDM5_%s_PASSWORD", toupper(dbType))))
-  sysServer <- Sys.getenv(sprintf("CDM5_%s_SERVER", toupper(dbType)))
-  sysExtraSettings <- Sys.getenv(sprintf("CDM5_%s_EXTRA_SETTINGS", toupper(dbType)))
-  if (sysUser != "" &
-      sysPassword != "" &
-      sysServer != "") {
-    cdmDatabaseSchema <- Sys.getenv(sprintf("CDM5_%s_CDM_SCHEMA", toupper(dbType)))
-    resultsDatabaseSchema <- Sys.getenv("CDM5_%s_OHDSI_SCHEMA", toupper(dbType))
-    
-    connectionDetails <- createConnectionDetails(dbms = dbType,
-                                       user = sysUser,
-                                       password = sysPassword,
-                                       server = sysServer,
-                                       extraSettings = sysExtraSettings)
   
-    results <- executeDqChecks(connectionDetails = connectionDetails, 
-                               cdmDatabaseSchema = cdmDatabaseSchema, 
-                               resultsDatabaseSchema = resultsDatabaseSchema, 
-                               cdmSourceName = "test", 
-                               numThreads = 1, 
-                               sqlOnly = FALSE, 
-                               outputFolder = "output", 
-                               verboseMode = FALSE, 
-                               writeToTable = FALSE, 
-                               checkLevels = c(), 
-                               checkNames = c())
+  dbTypes = c("oracle",
+              "postgresql",
+              "sql server")
+              
+  for (dbType in dbTypes) {
+    sysUser <- Sys.getenv(sprintf("CDM5_%s_USER", toupper(dbType)))
+    sysPassword <- URLdecode(Sys.getenv(sprintf("CDM5_%s_PASSWORD", toupper(dbType))))
+    sysServer <- Sys.getenv(sprintf("CDM5_%s_SERVER", toupper(dbType)))
+    sysExtraSettings <- Sys.getenv(sprintf("CDM5_%s_EXTRA_SETTINGS", toupper(dbType)))
+    if (sysUser != "" &
+        sysPassword != "" &
+        sysServer != "") {
+      cdmDatabaseSchema <- Sys.getenv(sprintf("CDM5_%s_CDM_SCHEMA", toupper(dbType)))
+      resultsDatabaseSchema <- Sys.getenv("CDM5_%s_OHDSI_SCHEMA", toupper(dbType))
+      
+      connectionDetails <- createConnectionDetails(dbms = dbType,
+                                         user = sysUser,
+                                         password = sysPassword,
+                                         server = sysServer,
+                                         extraSettings = sysExtraSettings)
     
-    expect_true(nrow(results$CheckResults) > 0)
+      results <- executeDqChecks(connectionDetails = connectionDetails, 
+                                 cdmDatabaseSchema = cdmDatabaseSchema, 
+                                 resultsDatabaseSchema = resultsDatabaseSchema, 
+                                 cdmSourceName = "test", 
+                                 numThreads = 1, 
+                                 sqlOnly = FALSE, 
+                                 outputFolder = "output", 
+                                 verboseMode = FALSE, 
+                                 writeToTable = FALSE, 
+                                 checkLevels = c(), 
+                                 checkNames = c())
+      
+      expect_true(nrow(results$CheckResults) > 0)
+    }
   }
 })
