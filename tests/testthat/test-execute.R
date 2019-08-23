3d8c638b6dcb62c8c87221ee832cf45527d4e6d4
Ajit Londhe <alondhe2@its.jnj.com>
2019-08-22 20:52:05 -0700

    Updated connection details for test

--- tests/testthat/test-execute.R
+++ tests/testthat/test-execute.R
@@ -1,17 +1,34 @@
 library(testthat)
 
 test_that("Execute DQ checks", {
-  results <- executeDqChecks(connectionDetails = connectionDetails, 
-                             cdmDatabaseSchema = cdmDatabaseSchema, 
-                             resultsDatabaseSchema = resultsDatabaseSchema, 
-                             cdmSourceName = "test", 
-                             numThreads = 1, 
-                             sqlOnly = FALSE, 
-                             outputFolder = "output", 
-                             verboseMode = FALSE, 
-                             writeToTable = FALSE, 
-                             checkLevels = c(), 
-                             checkNames = c())
+  sysUser <- Sys.getenv(sprintf("CDM5_%s_USER", toupper(dbType)))
+  sysPassword <- URLdecode(Sys.getenv(sprintf("CDM5_%s_PASSWORD", toupper(dbType))))
+  sysServer <- Sys.getenv(sprintf("CDM5_%s_SERVER", toupper(dbType)))
+  sysExtraSettings <- Sys.getenv(sprintf("CDM5_%s_EXTRA_SETTINGS", toupper(dbType)))
+  if (sysUser != "" &
+      sysPassword != "" &
+      sysServer != "") {
+    cdmDatabaseSchema <- Sys.getenv(sprintf("CDM5_%s_CDM_SCHEMA", toupper(dbType)))
+    resultsDatabaseSchema <- Sys.getenv("CDM5_%s_OHDSI_SCHEMA", toupper(dbType))
+    
+    connectionDetails <- createConnectionDetails(dbms = dbType,
+                                       user = sysUser,
+                                       password = sysPassword,
+                                       server = sysServer,
+                                       extraSettings = sysExtraSettings)
   
-  expect_true(nrow(results$CheckResults) > 0)
+    results <- executeDqChecks(connectionDetails = connectionDetails, 
+                               cdmDatabaseSchema = cdmDatabaseSchema, 
+                               resultsDatabaseSchema = resultsDatabaseSchema, 
+                               cdmSourceName = "test", 
+                               numThreads = 1, 
+                               sqlOnly = FALSE, 
+                               outputFolder = "output", 
+                               verboseMode = FALSE, 
+                               writeToTable = FALSE, 
+                               checkLevels = c(), 
+                               checkNames = c())
+    
+    expect_true(nrow(results$CheckResults) > 0)
+  }
 })
