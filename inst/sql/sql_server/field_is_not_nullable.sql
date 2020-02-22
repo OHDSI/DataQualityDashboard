37abb0747d0f4f5b11ac9f665ac78625a0d64e82
alondhe <alondhe@amgen.com>
2020-02-22 11:19:23 -0800

    Fixes #93

--- inst/sql/sql_server/field_is_not_nullable.sql
+++ inst/sql/sql_server/field_is_not_nullable.sql
@@ -15,7 +15,7 @@ SELECT num_violated_rows, CASE WHEN denominator.num_rows = 0 THEN 0 ELSE 1.0*num
   denominator.num_rows as num_denominator_rows
 FROM
 (
-	SELECT COUNT(violated_rows.violating_field) AS num_violated_rows
+	SELECT COUNT_BIG(violated_rows.violating_field) AS num_violated_rows
 	FROM
 	(
 		SELECT '@cdmTableName.@cdmFieldName' AS violating_field, A.* 
@@ -24,7 +24,7 @@ FROM
 	) violated_rows
 ) violated_row_count,
 ( 
-	SELECT COUNT(*) AS num_rows
+	SELECT COUNT_BIG(*) AS num_rows
 	FROM @cdmDatabaseSchema.@cdmTableName
 ) denominator
 ;
