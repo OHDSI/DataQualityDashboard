ad6137cf383289fd5d98d8b929ccf2e02fafc84e
Chudakov Matvey <morganrush@yandex.ru>
2021-01-25 17:13:24 +0300

    data quality dashboard service

---
+++ src/main/java/com/arcadia/DataQualityDashboard/DataQualityDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.arcadia.DataQualityDashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataQualityDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataQualityDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arcadia/DataQualityDashboard/DataQualityDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcadia.DataQualityDashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataQualityDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
