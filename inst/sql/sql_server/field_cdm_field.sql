ca22085eaf1a033d93e74ce7474c8f46dc864b15
Clair Blacketer <mblacke@its.jnj.com>
2021-03-02 09:50:00 -0500

    Remove quotes from CDM_FIELD.sql
    Closes #205. Tested on PDW, Redshift, and Postgresql

--- inst/sql/sql_server/field_cdm_field.sql
+++ inst/sql/sql_server/field_cdm_field.sql
@@ -18,7 +18,7 @@ FROM
   select num_violated_rows from
   (
     select
-      case when count_big("@cdmFieldName") = 0 then 0
+      case when count_big(@cdmFieldName) = 0 then 0
       else 0
     end as num_violated_rows
     from @cdmDatabaseSchema.@cdmTableName cdmTable
