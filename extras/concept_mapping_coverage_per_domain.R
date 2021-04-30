a2b4bb7ee468f9bac8c6b61301eb6e0cec7e958b
Maxim Moinat <maxim@thehyve.nl>
2021-04-30 19:10:42 +0200

    output unmapped pct and count as table

--- extras/concept_mapping_coverage_per_domain.R
+++ extras/concept_mapping_coverage_per_domain.R
@@ -2,10 +2,12 @@ library("dplyr")
 library("jsonlite")
 library("ggplot2")
 
-path_to_dqd_file <- '/my/path/to/dqd.json'
+setwd('/my/path/to/dqd/')
+
+dqd_filename <- 'dqd.json'
 
 # Load data
-result <- jsonlite::fromJSON(path_to_dqd_file)
+result <- jsonlite::fromJSON(dqd_filename)
 check_results <- result$CheckResults %>%
   select(CHECK_NAME, CDM_TABLE_NAME, CDM_FIELD_NAME, 
          NUM_VIOLATED_ROWS, NUM_DENOMINATOR_ROWS, PCT_VIOLATED_ROWS)
@@ -13,11 +15,10 @@ check_results <- result$CheckResults %>%
 # Add fields
 coverage_results <- check_results %>%
   filter(CHECK_NAME %in% c("standardConceptRecordCompleteness", "sourceValueCompleteness")) %>%
-  filter(!(CDM_TABLE_NAME %in% c("DRUG_ERA", "DOSE_ERA", "CONDITION_ERA"))) %>%
   mutate(
     # Coverage is rows not failing
     coveragePct = 1 - PCT_VIOLATED_ROWS,
-    # Standardize domain by taking the base of the field names
+    # Base of the field names is a proxy for the domain
     domain = ifelse(CHECK_NAME=="standardConceptRecordCompleteness", 
                       sub('_CONCEPT_ID', '', CDM_FIELD_NAME), 
                       sub('_SOURCE_VALUE', '', CDM_FIELD_NAME)
@@ -27,19 +28,37 @@ coverage_results <- check_results %>%
                    standardConceptRecordCompleteness = "Records",
                    sourceValueCompleteness = "Terms")
   ) %>%
+  # To keep things simple, we only look at the six main domains (and exluce era's)
+  filter(!(CDM_TABLE_NAME %in% c("DRUG_ERA", "DOSE_ERA", "CONDITION_ERA"))) %>%
   filter(domain %in% c("VISIT", "PROCEDURE", "DRUG", "CONDITION", "MEASUREMENT", "OBSERVATION"))
 
+# Table
+table <- coverage_results %>% 
+  mutate(
+    percentUnmapped = scales::percent(PCT_VIOLATED_ROWS, accuracy = 0.01),
+    nUnmapped = formatC(NUM_VIOLATED_ROWS, format="d", big.mark=","),
+    nTotal = formatC(NUM_DENOMINATOR_ROWS, format="d", big.mark=",")
+  ) %>%
+  select(domain, coverageType, percentUnmapped, nUnmapped, nTotal) %>% 
+  arrange(domain, desc(coverageType))  # by domain, terms first, then records
+
+ write.csv(table, file="concept_mapping_coverage.csv", row.names = FALSE)
+
 # Coverage plot like fig 6 in EHDEN DoA
+# note: coverage is percentage NOT failing to map
 coverage_results %>%
+  mutate(
+    coveragePct = 1 - PCT_VIOLATED_ROWS
+  ) %>%
   ggplot(aes(x=coverageType, y = coveragePct, fill = coverageType)) +
   geom_col() +
   geom_text(aes(label=scales::percent(coveragePct, accuracy = 0.01)), 
             position=position_stack(vjust=0.5), 
-            size=3, colour="white") +
+            size=3, colour="white", fontface="bold") +
   theme(
     axis.text.y=element_text(size=10),
     strip.placement="outside",
-    strip.text.y=element_text(angle=90, hjust=0.5, face="bold", size=10,
+    strip.text.y=element_text(angle=90, hjust=0.5, face="bold", size=6,
                               margin=margin(r=0))
   ) +
   coord_flip() +
@@ -49,3 +68,6 @@ coverage_results %>%
   ylab("Percentage Coverage (%)") + 
   xlab("") + 
   scale_fill_manual(values=c("darkblue", "skyblue"))
+
+ggsave('concept_mapping_coverage.png', height = 5, width = 5 * 1.61803)
+ 
