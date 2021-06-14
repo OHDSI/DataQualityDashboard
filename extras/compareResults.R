446acd1d1b3b1fdaa8bf293ccea3d56177c8608e
Maxim Moinat <maxim@thehyve.nl>
2021-06-14 14:50:20 +0200

    code cleanup

--- extras/compareResults.R
+++ extras/compareResults.R
@@ -14,50 +14,39 @@ dir.create(file.path(getwd(), saving_dir), showWarnings = FALSE)
 # List all differences
 # ... between OLD
 result_old <- jsonlite::fromJSON(file_old)
-
-check_results_old <- result_old$CheckResults %>%
-  select(checkId, CHECK_NAME, 
-         CDM_TABLE_NAME, CDM_FIELD_NAME, CONCEPT_ID, UNIT_CONCEPT_ID,
-         THRESHOLD_VALUE, PCT_VIOLATED_ROWS,
-         FAILED)
+check_results_old <- tibble(result_old$CheckResults)
 
 # ... and NEW
 result_new <- jsonlite::fromJSON(file_new)
-
-check_results_new <- result_new$CheckResults %>%
-  select(checkId, CHECK_NAME, 
-         CDM_TABLE_NAME, CDM_FIELD_NAME, CONCEPT_ID, UNIT_CONCEPT_ID,
-         THRESHOLD_VALUE, PCT_VIOLATED_ROWS,
-         FAILED) %>%
-  rename(new_PCT_VIOLATED_ROWS = PCT_VIOLATED_ROWS)
+check_results_new <- tibble(result_new$CheckResults)
 
 # ... only keep the different
-combined_results <- tibble(check_results_old) %>%
-  left_join(check_results_new, by=c("CDM_TABLE_NAME", "CDM_FIELD_NAME",
-                                    "CHECK_NAME", "CONCEPT_ID", "UNIT_CONCEPT_ID")) %>%
-  filter(PCT_VIOLATED_ROWS != new_PCT_VIOLATED_ROWS)
+combined_results <- check_results_old %>%
+  left_join(check_results_new,
+            by=c("CHECK_NAME", "CDM_TABLE_NAME", "CDM_FIELD_NAME",
+                 "CONCEPT_ID", "UNIT_CONCEPT_ID"),
+            suffix=c(".old", ".new")) %>%
+  filter(PCT_VIOLATED_ROWS.old != PCT_VIOLATED_ROWS.new)
 
 # Save as csv
 write.csv(combined_results, file=file.path(getwd(), saving_dir, "different_checks.csv"))
 
-
-# Plot them - interactive
+# Plot
 p <- combined_results %>%
   mutate(       
-    # Otherwise legend names are overwritten by ggplotly
-    fails = ifelse(FAILED.y==0, "Pass", "Fail"),
-    # for aesthetic of labels
-    pct_old = round(PCT_VIOLATED_ROWS*100, digits=2),
-    pct_new = round(new_PCT_VIOLATED_ROWS*100, digits=2)
+    fail_status = ifelse(FAILED.new==0, "Pass", "Fail"),
+    pct_old = round(PCT_VIOLATED_ROWS.old*100, digits=2),
+    pct_new = round(PCT_VIOLATED_ROWS.new*100, digits=2)
   ) %>% 
-  ggplot(aes(x=PCT_VIOLATED_ROWS*100, y=new_PCT_VIOLATED_ROWS*100,
-             colour=fails,
+  ggplot(aes(x=pct_old,
+             y=pct_new,
+             colour=fail_status,
              text=paste(
-               sprintf('<br><i>Check ID: </i>%s', checkId.y),
+               sprintf('<br><i>Check ID: </i>%s', checkId.new),
                sprintf('<br><i>Check name: </i>%s', CHECK_NAME),
                sprintf('<br><i>Table: </i>%s', CDM_TABLE_NAME),
                sprintf('<br><i>Field: </i>%s', CDM_FIELD_NAME),
-               sprintf('<br><i>Threshold value: </i>%.1f%%', THRESHOLD_VALUE.y),
+               sprintf('<br><i>Threshold value: </i>%.1f%%', THRESHOLD_VALUE.new),
                sprintf('<br><b><i>old: </i> %.2f%% </b>', pct_old),
                sprintf('<br><b><i>new: </i> %.2f%% </b>', pct_new)
              ), alpha=0.6)) +
@@ -68,14 +57,13 @@ p <- combined_results %>%
   scale_alpha(guide = 'none') +
   theme_minimal() +
   theme(legend.title = element_blank()) +
-  labs(x="Previous % of row fails", y="Current % of row fails") +
   expand_limits(y=100, x=100) +
-  annotate("text",label="Improved", x = 90, y = 10, colour="grey") +
-  annotate("text",label="Worsened", x = 10, y = 90, colour="grey")
+  labs(x="Previous % of row fails", y="Current % of row fails") +
+  annotate("text",label="Improved", x = 88.5, y = 12.5, colour="grey") +
+  annotate("text",label="Worsened", x = 12.5, y = 88.5, colour="grey")
 
-
-p2 <- ggplotly(p, tooltip="text") %>%
+p_interactive <- ggplotly(p, tooltip="text") %>%
   style(hoveron="text")
 
-htmlwidgets::saveWidget(p2, file.path(getwd(), saving_dir,"fig_compare_DQD.html"))
-p2
+htmlwidgets::saveWidget(p_interactive, file.path(getwd(), saving_dir, "fig_compare_DQD.html"))
+p_interactive
