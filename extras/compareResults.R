2036b54f61ac1af4c0f6fa27813ba1fb07d75cb3
egarcialara <e.oohla@hotmail.com>
2021-06-21 16:53:25 +0200

    remove ggplot2::

--- extras/compareResults.R
+++ extras/compareResults.R
@@ -4,7 +4,8 @@
 #' compare_results(file_old, file_new, saving_dir)
 
 library("dplyr")
-# Other packages used: jsonlite, ggplot2, plotly
+library("ggplot2")
+# Other packages used: jsonlite, plotly
 
 compare_results <- function(jsonPath.old, jsonPath.new, saving_dir){
 
@@ -42,7 +43,7 @@ compare_results <- function(jsonPath.old, jsonPath.new, saving_dir){
       pct_old = round(PCT_VIOLATED_ROWS.old*100, digits=2),
       pct_new = round(PCT_VIOLATED_ROWS.new*100, digits=2)
     ) %>% 
-    ggplot2::ggplot(ggplot2::aes(x=pct_old,
+    ggplot(aes(x=pct_old,
                y=pct_new,
                colour=fail_status,
                text=paste(
@@ -53,17 +54,17 @@ compare_results <- function(jsonPath.old, jsonPath.new, saving_dir){
                  sprintf('<br><b><i>old: </i> %.2f%% </b>', pct_old),
                  sprintf('<br><b><i>new: </i> %.2f%% </b>', pct_new)
                ), alpha=0.6)) +
-    ggplot2::geom_point() +
-    ggplot2::geom_abline(colour="gray", linetype = "dashed")+
-    ggplot2::scale_colour_manual(labels = c("Fail", "Pass"), 
+    geom_point() +
+    geom_abline(colour="gray", linetype = "dashed")+
+    scale_colour_manual(labels = c("Fail", "Pass"), 
                         values = c("Pass" = "darkblue", "Fail" = "chocolate1"))+
-    ggplot2::scale_alpha(guide = 'none') +
-    ggplot2::theme_minimal() +
-    ggplot2::theme(legend.title = ggplot2::element_blank()) +
-    ggplot2::lims(y=c(0,100), x=c(0,100)) +
-    ggplot2::labs(x="Previous % of row fails", y="Current % of row fails") +
-    ggplot2::annotate("text", label="Improved", x = 88.5, y = 12.5, colour="grey") +
-    ggplot2::annotate("text", label="Worsened", x = 12.5, y = 88.5, colour="grey")
+    scale_alpha(guide = 'none') +
+    theme_minimal() +
+    theme(legend.title = element_blank()) +
+    lims(y=c(0,100), x=c(0,100)) +
+    labs(x="Previous % of row fails", y="Current % of row fails") +
+    annotate("text", label="Improved", x = 88.5, y = 12.5, colour="grey") +
+    annotate("text", label="Worsened", x = 12.5, y = 88.5, colour="grey")
   
   p_interactive <- plotly::ggplotly(p, tooltip="text") %>%
     plotly::style(hoveron="text")
