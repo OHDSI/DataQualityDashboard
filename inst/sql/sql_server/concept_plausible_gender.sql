34de580c8cd73b788cc7f63c9cf664852856b554
amolin19 <amolin19@its.jnj.com>
2021-08-31 16:42:19 -0400

    cleansing plausble values - only updating, not deleting for plausible hi/lo values

--- inst/sql/sql_server/concept_plausible_gender.sql
+++ inst/sql/sql_server/concept_plausible_gender.sql
@@ -33,38 +33,38 @@ cohortDatabaseSchema = @cohortDatabaseSchema
     );	
 }
 {@EXECUTE} ? {
-SELECT num_violated_rows, CASE WHEN denominator.num_rows = 0 THEN 0 ELSE 1.0*num_violated_rows/denominator.num_rows END AS pct_violated_rows, 
-  denominator.num_rows as num_denominator_rows
-FROM
-(
-	SELECT COUNT_BIG(*) AS num_violated_rows
+	SELECT num_violated_rows, CASE WHEN denominator.num_rows = 0 THEN 0 ELSE 1.0*num_violated_rows/denominator.num_rows END AS pct_violated_rows, 
+	  denominator.num_rows as num_denominator_rows
 	FROM
 	(
-		SELECT cdmTable.* 
+		SELECT COUNT_BIG(*) AS num_violated_rows
+		FROM
+		(
+			SELECT cdmTable.* 
+			FROM @cdmDatabaseSchema.@cdmTableName cdmTable
+				INNER JOIN @cdmDatabaseSchema.person p
+				ON cdmTable.person_id = p.person_id
+				
+				{@cohort}?{
+			JOIN @cohortDatabaseSchema.COHORT c
+			ON cdmTable.PERSON_ID = c.SUBJECT_ID
+			AND c.COHORT_DEFINITION_ID = @cohortDefinitionId
+			}
+				
+			WHERE cdmTable.@cdmFieldName = @conceptId
+			AND p.gender_concept_id <> {@plausibleGender == 'Male'} ? {8507} : {8532} 
+		) violated_rows
+	) violated_row_count,
+	( 
+		SELECT COUNT_BIG(*) AS num_rows
 		FROM @cdmDatabaseSchema.@cdmTableName cdmTable
-			INNER JOIN @cdmDatabaseSchema.person p
-			ON cdmTable.person_id = p.person_id
-			
-			{@cohort}?{
-    	JOIN @cohortDatabaseSchema.COHORT c
-    	ON cdmTable.PERSON_ID = c.SUBJECT_ID
-    	AND c.COHORT_DEFINITION_ID = @cohortDefinitionId
-    	}
-			
-		WHERE cdmTable.@cdmFieldName = @conceptId
-		AND p.gender_concept_id <> {@plausibleGender == 'Male'} ? {8507} : {8532} 
-	) violated_rows
-) violated_row_count,
-( 
-	SELECT COUNT_BIG(*) AS num_rows
-	FROM @cdmDatabaseSchema.@cdmTableName cdmTable
-	
-	{@cohort}?{
-	JOIN @cohortDatabaseSchema.COHORT c
-	ON cdmTable.PERSON_ID = c.SUBJECT_ID
-	AND c.COHORT_DEFINITION_ID = @cohortDefinitionId
-	}
-	
-	WHERE @cdmFieldName = @conceptId
-) denominator;
+		
+		{@cohort}?{
+		JOIN @cohortDatabaseSchema.COHORT c
+		ON cdmTable.PERSON_ID = c.SUBJECT_ID
+		AND c.COHORT_DEFINITION_ID = @cohortDefinitionId
+		}
+		
+		WHERE @cdmFieldName = @conceptId
+	) denominator;
 }
--- inst/sql/sql_server/concept_plausible_value_low.sql
+++ inst/sql/sql_server/concept_plausible_value_low.sql
@@ -15,37 +15,53 @@ cohortDatabaseSchema = @cohortDatabaseSchema
 }
 **********/
 
-
-SELECT num_violated_rows, CASE WHEN denominator.num_rows = 0 THEN 0 ELSE 1.0*num_violated_rows/denominator.num_rows END  AS pct_violated_rows, 
-  denominator.num_rows as num_denominator_rows
-FROM
-(
-	SELECT COUNT_BIG(*) AS num_violated_rows
-	FROM
-	(
+{@CLEANSE} ? {
+	INSERT INTO @cdmDatabaseSchema.@cdmTableName_archive
 		SELECT m.* 
 		FROM @cdmDatabaseSchema.@cdmTableName m
-		{@cohort}?{
-  	JOIN @cohortDatabaseSchema.COHORT c
-  	ON m.PERSON_ID = c.SUBJECT_ID
-  	AND c.COHORT_DEFINITION_ID = @cohortDefinitionId
-  	}
 		WHERE m.@cdmFieldName = @conceptId
 		AND m.unit_concept_id = @unitConceptId
 		AND m.value_as_number IS NOT NULL
-		AND m.value_as_number < @plausibleValueLow
-	) violated_rows
-) violated_row_count,
-( 
-	SELECT COUNT_BIG(*) AS num_rows
-	FROM @cdmDatabaseSchema.@cdmTableName m
-	{@cohort}?{
-	JOIN @cohortDatabaseSchema.COHORT c
-	ON m.PERSON_ID = c.SUBJECT_ID
-	AND c.COHORT_DEFINITION_ID = @cohortDefinitionId
-	}
-	WHERE m.@cdmFieldName = @conceptId
-	AND unit_concept_id = @unitConceptId
-	AND value_as_number IS NOT NULL
-) denominator
-;
+		AND m.value_as_number < @plausibleValueHigh; 
+	
+	UPDATE @cdmDatabaseSchema.@cdmTableName SET value_as_number = NULL 
+		WHERE m.@cdmFieldName = @conceptId
+		AND m.unit_concept_id = @unitConceptId
+		AND m.value_as_number IS NOT NULL
+		AND m.value_as_number < @plausibleValueHigh;	
+}
+
+{@EXECUTE} ? {
+	SELECT num_violated_rows, CASE WHEN denominator.num_rows = 0 THEN 0 ELSE 1.0*num_violated_rows/denominator.num_rows END  AS pct_violated_rows, 
+	  denominator.num_rows as num_denominator_rows
+	FROM
+	(
+		SELECT COUNT_BIG(*) AS num_violated_rows
+		FROM
+		(
+			SELECT m.* 
+			FROM @cdmDatabaseSchema.@cdmTableName m
+			{@cohort}?{
+		JOIN @cohortDatabaseSchema.COHORT c
+		ON m.PERSON_ID = c.SUBJECT_ID
+		AND c.COHORT_DEFINITION_ID = @cohortDefinitionId
+		}
+			WHERE m.@cdmFieldName = @conceptId
+			AND m.unit_concept_id = @unitConceptId
+			AND m.value_as_number IS NOT NULL
+			AND m.value_as_number < @plausibleValueLow
+		) violated_rows
+	) violated_row_count,
+	( 
+		SELECT COUNT_BIG(*) AS num_rows
+		FROM @cdmDatabaseSchema.@cdmTableName m
+		{@cohort}?{
+		JOIN @cohortDatabaseSchema.COHORT c
+		ON m.PERSON_ID = c.SUBJECT_ID
+		AND c.COHORT_DEFINITION_ID = @cohortDefinitionId
+		}
+		WHERE m.@cdmFieldName = @conceptId
+		AND unit_concept_id = @unitConceptId
+		AND value_as_number IS NOT NULL
+	) denominator;
+}
