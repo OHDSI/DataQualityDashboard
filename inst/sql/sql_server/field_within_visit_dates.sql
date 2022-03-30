e19993c8c9b4ead301c2bf1664772d87a0c95d83
AnthonyMolinaro <amolin19@its.jnj.com>
2022-03-30 16:09:42 -0400

    Addes field level check to determine if events occur within their corresponding visit start and end dates. Introduced in PR #178

---
+++ inst/sql/sql_server/field_within_visit_dates.sql
@@ -0,0 +1,46 @@
+/*********
+FIELD LEVEL check:
+WITHIN_VISIT_DATES - find events that occur before the corresponding visit_start_date or after the corresponding visit_end_date
+
+Parameters used in this template:
+cdmDatabaseSchema = @cdmDatabaseSchema
+cdmTableName = @cdmTableName
+cdmFieldName = @cdmFieldName
+{@cohort & '@runForCohort' == 'Yes'}?{
+cohortDefinitionId = @cohortDefinitionId
+cohortDatabaseSchema = @cohortDatabaseSchema
+}
+**********/
+
+
+SELECT num_violated_rows, 
+       CASE WHEN denominator.num_rows = 0 THEN 0 ELSE 1.0*num_violated_rows/denominator.num_rows END  AS pct_violated_rows, 
+       denominator.num_rows as num_denominator_rows
+FROM
+(
+	SELECT COUNT_BIG(violated_rows.violating_field) AS num_violated_rows
+	FROM
+	(
+		SELECT '@cdmTableName.@cdmFieldName' AS violating_field, cdmTable.*
+          FROM @cdmDatabaseSchema.@cdmTableName cdmTable
+          {@cohort & '@runForCohort' == 'Yes'}?{
+    	  JOIN @cohortDatabaseSchema.cohort c 
+    	    ON cdmTable.person_id = c.subject_id
+    	   AND c.cohort_definition_id = @cohortDefinitionId
+    	  }
+          JOIN @cdmDatabaseSchema.visit_occurrence vo
+            ON cdmTable.visit_occurrence_id = vo.visit_occurrence_id
+         WHERE cdmTable.@cdmFieldName < vo.visit_start_date
+            OR cdmTable.@cdmFieldName > vo.visit_end_date 
+	) violated_rows
+) violated_row_count,
+(
+	SELECT COUNT_BIG(*) AS num_rows
+	FROM @cdmDatabaseSchema.@cdmTableName cdmTable
+	{@cohort & '@runForCohort' == 'Yes'}?{
+    JOIN @cohortDatabaseSchema.cohort c 
+      ON cdmTable.person_id = c.subject_id
+     AND c.cohort_definition_id = @cohortDefinitionId
+    }
+) denominator
+;
