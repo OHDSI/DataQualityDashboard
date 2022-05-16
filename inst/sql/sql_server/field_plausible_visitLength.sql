53cad4293ff09602b34b1040e6ba21b120a5760d
priagopal <31636593+priagopal@users.noreply.github.com>
2022-05-16 14:39:16 -0400

    Update field_plausible_visitLength.sql

--- inst/sql/sql_server/field_plausible_visitLength.sql
+++ inst/sql/sql_server/field_plausible_visitLength.sql
@@ -11,15 +11,34 @@ cohortDefinitionId = @cohortDefinitionId
 cohortDatabaseSchema = @cohortDatabaseSchema
 }
 **********/
-
 SELECT num_violated_rows, CASE WHEN denominator.num_rows = 0 THEN 0 ELSE 1.0*num_violated_rows/denominator.num_rows END  AS pct_violated_rows, 
   denominator.num_rows as num_denominator_rows
 FROM
 (
-	SELECT COUNT_BIG(violated_rows.violating_field) AS num_violated_rows
+	SELECT COUNT(*) AS num_violated_rows
 	FROM
 	(
-		SELECT '@cdmTable.cdmFieldName' as violating_field, cdmTable.*
+		SELECT  cdmTable.*
+	FROM cdm_synthea_v1.visit_occurrence cdmTable
+    WHERE  (cdmTable.visit_concept_id =9203 AND
+          (date_part('day',cdmTable.visit_end_date) - date_part('day',cdmTable.visit_start_date)) >2)
+          OR  (cdmTable.visit_concept_id = 9201 AND
+           (date_part('day',cdmTable.visit_end_date) - date_part('day',cdmTable.visit_start_date)) >1)
+	) violated_rows
+) violated_row_count,
+(
+	SELECT COUNT(*) AS num_rows
+	FROM cdm_synthea_v1.visit_occurrence cdmTable
+   ) denominator
+;
+SELECT num_violated_rows, CASE WHEN denominator.num_rows = 0 THEN 0 ELSE 1.0*num_violated_rows/denominator.num_rows END  AS pct_violated_rows, 
+  denominator.num_rows as num_denominator_rows
+FROM
+(
+	SELECT COUNT(*) AS num_violated_rows
+	FROM
+	(
+		SELECT  cdmTable.*
 	FROM @CdmDatabaseSchema.@cdmTableName cdmTable
         {@cohort & '@runForCohort' == 'Yes'}?{
          JOIN @cohortDatabaseSchema.cohort c
@@ -33,7 +52,7 @@ FROM
 	) violated_rows
 ) violated_row_count,
 (
-	SELECT COUNT_BIG(*) AS num_rows
+	SELECT COUNT(*) AS num_rows
 	FROM @cdmDatabaseSchema.@cdmTableName cdmTable
 	{@cohort & '@runForCohort' == 'Yes'}?{
   JOIN @cohortDatabaseSchema.COHORT c 
