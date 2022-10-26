65539c45d7778feab34dcb1dc124968143d56c0c
Matvey Chudakov <matvey159@gmail.com>
2022-10-26 11:10:25 +0300

    Fixed test

--- src/test/java/com/arcadia/DataQualityDashboard/util/RConnectionWrapperUtilTest.java
+++ src/test/java/com/arcadia/DataQualityDashboard/util/RConnectionWrapperUtilTest.java
@@ -1,22 +1,42 @@
 package com.arcadia.DataQualityDashboard.util;
 
 import com.arcadia.DataQualityDashboard.model.DataQualityScan;
+import com.arcadia.DataQualityDashboard.model.DbSettings;
 import org.junit.jupiter.api.Test;
 
-import static com.arcadia.DataQualityDashboard.service.DataQualityServiceTest.createTestScan;
+import static com.arcadia.DataQualityDashboard.model.ScanStatus.IN_PROGRESS;
 import static com.arcadia.DataQualityDashboard.service.TestProperties.dqdDatabaseProperties;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RConnectionWrapperUtilTest {
     @Test
     void createDataQualityCheckCommand() {
-        DataQualityScan scan = createTestScan();
+        DbSettings dbSettings = DbSettings.builder()
+                .dbType("sql server")
+                .server("localhost")
+                .port(1433)
+                .database("cdm_test_53")
+                .schema("dbo")
+                .user("test")
+                .password("test")
+                .build();
+        DataQualityScan dataQualityScan = DataQualityScan.builder()
+                .id(1L)
+                .username("Perseus")
+                .project("Test")
+                .statusCode(IN_PROGRESS.getCode())
+                .statusName(IN_PROGRESS.getName())
+                .dbSettings(dbSettings)
+                .build();
+        dbSettings.setDataQualityScan(dataQualityScan);
+
         String dbType = "postgresql";
         String server = "localhost";
         String schema = "test";
         int threadCount = 1;
+
         String command = RConnectionWrapperUtil.createDataQualityCheckCommand(
-                scan,
+                dataQualityScan,
                 dbType,
                 server,
                 schema,
@@ -28,13 +48,13 @@ class RConnectionWrapperUtilTest {
                                 "cdm_server = \"localhost\", " +
                                 "cdm_port = 1433, " +
                                 "cdm_dataBaseSchema = \"test\", " +
-                                "cdm_user = \"cdm_builder\", " +
-                                "cdm_password = \"builder1!\", " +
+                                "cdm_user = \"test\", " +
+                                "cdm_password = \"test\", " +
                                 "scanId = 1, " +
                                 "threadCount = 1, " +
-                                "cdmSourceName = \"Data Quality\", " +
+                                "cdmSourceName = \"Test\", " +
                                 "dqd_dataType = \"postgresql\", " +
-                                "dqd_server = \"localhost/shared\", " +
+                                "dqd_server = \"shareddb/shared\", " +
                                 "dqd_port = 5432, " +
                                 "dqd_dataBaseSchema = \"dqd\", " +
                                 "dqd_user = \"dqd\", " +
