1daf3c6c08c313f8b900b3743b24b8cb9127699c
Katy Sadowski <katysadowski@Katys-MBP-2.lan>
2023-07-11 10:55:55 -0400

    add new test structure

--- tests/testthat/test-executeDqChecks.R
+++ tests/testthat/test-executeDqChecks.R
@@ -1,12 +1,6 @@
 library(testthat)
 local_edition(3)
 
-test_that("listDqChecks works", {
-  checks <- listDqChecks()
-  expect_equal(length(checks), 4)
-  expect_true(all(sapply(checks, is.data.frame)))
-})
-
 test_that("Execute a single DQ check on Synthea/Eunomia", {
   outputFolder <- tempfile("dqd_")
   on.exit(unlink(outputFolder, recursive = TRUE))
@@ -43,7 +37,6 @@ test_that("Execute all TABLE checks on Synthea/Eunomia", {
 
   expect_true(nrow(results$CheckResults) > 0)
 })
-
 
 test_that("Execute FIELD checks on Synthea/Eunomia", {
   outputFolder <- tempfile("dqd_")
@@ -119,54 +112,54 @@ test_that("Execute a single DQ check on a cohort in Synthea/Eunomia", {
   )
 })
 
-test_that("Execute a single DQ check on remote databases", {
-  outputFolder <- tempfile("dqd_")
-  on.exit(unlink(outputFolder, recursive = TRUE))
-
-  dbTypes <- c(
-    "oracle",
-    "postgresql",
-    "sql server"
-  )
-
-  for (dbType in dbTypes) {
-    sysUser <- Sys.getenv(sprintf("CDM5_%s_USER", toupper(gsub(" ", "_", dbType))))
-    sysPassword <- URLdecode(Sys.getenv(sprintf("CDM5_%s_PASSWORD", toupper(gsub(" ", "_", dbType)))))
-    sysServer <- Sys.getenv(sprintf("CDM5_%s_SERVER", toupper(gsub(" ", "_", dbType))))
-    if (sysUser != "" &
-      sysPassword != "" &
-      sysServer != "") {
-      cdmDatabaseSchema <- Sys.getenv(sprintf("CDM5_%s_CDM_SCHEMA", toupper(gsub(" ", "_", dbType))))
-      resultsDatabaseSchema <- Sys.getenv("CDM5_%s_OHDSI_SCHEMA", toupper(gsub(" ", "_", dbType)))
-
-      connectionDetails <- createConnectionDetails(
-        dbms = dbType,
-        user = sysUser,
-        password = sysPassword,
-        server = sysServer,
-        pathToDriver = jdbcDriverFolder
-      )
-
-      expect_warning(
-        results <- executeDqChecks(
-          connectionDetails = connectionDetails,
-          cdmDatabaseSchema = cdmDatabaseSchema,
-          resultsDatabaseSchema = resultsDatabaseSchema,
-          cdmSourceName = "test",
-          numThreads = 1,
-          sqlOnly = FALSE,
-          outputFolder = outputFolder,
-          verboseMode = FALSE,
-          writeToTable = FALSE,
-          checkNames = "measurePersonCompleteness"
-        ),
-        regexp = "^Missing check names.*"
-      )
-
-      expect_true(nrow(results$CheckResults) > 0)
-    }
-  }
-})
+# test_that("Execute a single DQ check on remote databases", {
+#   outputFolder <- tempfile("dqd_")
+#   on.exit(unlink(outputFolder, recursive = TRUE))
+# 
+#   dbTypes <- c(
+#     "oracle",
+#     "postgresql",
+#     "sql server"
+#   )
+# 
+#   for (dbType in dbTypes) {
+#     sysUser <- Sys.getenv(sprintf("CDM5_%s_USER", toupper(gsub(" ", "_", dbType))))
+#     sysPassword <- URLdecode(Sys.getenv(sprintf("CDM5_%s_PASSWORD", toupper(gsub(" ", "_", dbType)))))
+#     sysServer <- Sys.getenv(sprintf("CDM5_%s_SERVER", toupper(gsub(" ", "_", dbType))))
+#     if (sysUser != "" &
+#       sysPassword != "" &
+#       sysServer != "") {
+#       cdmDatabaseSchema <- Sys.getenv(sprintf("CDM5_%s_CDM_SCHEMA", toupper(gsub(" ", "_", dbType))))
+#       resultsDatabaseSchema <- Sys.getenv("CDM5_%s_OHDSI_SCHEMA", toupper(gsub(" ", "_", dbType)))
+# 
+#       connectionDetails <- createConnectionDetails(
+#         dbms = dbType,
+#         user = sysUser,
+#         password = sysPassword,
+#         server = sysServer,
+#         pathToDriver = jdbcDriverFolder
+#       )
+# 
+#       expect_warning(
+#         results <- executeDqChecks(
+#           connectionDetails = connectionDetails,
+#           cdmDatabaseSchema = cdmDatabaseSchema,
+#           resultsDatabaseSchema = resultsDatabaseSchema,
+#           cdmSourceName = "test",
+#           numThreads = 1,
+#           sqlOnly = FALSE,
+#           outputFolder = outputFolder,
+#           verboseMode = FALSE,
+#           writeToTable = FALSE,
+#           checkNames = "measurePersonCompleteness"
+#         ),
+#         regexp = "^Missing check names.*"
+#       )
+# 
+#       expect_true(nrow(results$CheckResults) > 0)
+#     }
+#   }
+# })
 
 test_that("Check invalid cdm version", {
   outputFolder <- tempfile("dqd_")
@@ -184,44 +177,6 @@ test_that("Check invalid cdm version", {
     ),
     regexp = "^cdmVersion must contain a version of the form '5.X'"
   )
-})
-
-test_that("Write JSON results", {
-  outputFolder <- tempfile("dqd_")
-  on.exit(unlink(outputFolder, recursive = TRUE))
-
-  expect_warning(
-    results <- executeDqChecks(
-      connectionDetails = connectionDetailsEunomia,
-      cdmDatabaseSchema = cdmDatabaseSchemaEunomia,
-      resultsDatabaseSchema = resultsDatabaseSchemaEunomia,
-      cdmSourceName = "Eunomia",
-      checkNames = "measurePersonCompleteness",
-      outputFolder = outputFolder,
-      writeToTable = FALSE
-    ),
-    regexp = "^Missing check names.*"
-  )
-
-  jsonPath <- list.files(outputFolder, ".json", full.names = TRUE)
-  csvPath <- file.path(outputFolder, "results.csv")
-  writeJsonResultsToCsv(
-    jsonPath = jsonPath,
-    csvPath = csvPath
-  )
-  expect_true(file.exists(csvPath))
-
-  DataQualityDashboard::writeJsonResultsToTable(
-    connectionDetails = connectionDetailsEunomia,
-    resultsDatabaseSchema = resultsDatabaseSchemaEunomia,
-    jsonFilePath = jsonPath,
-    writeTableName = "dqd_results"
-  )
-  connection <- DatabaseConnector::connect(connectionDetailsEunomia)
-  on.exit(DatabaseConnector::disconnect(connection), add = TRUE)
-  tableNames <- DatabaseConnector::getTableNames(connection = connection, databaseSchema = resultsDatabaseSchemaEunomia)
-  expect_true("dqd_results" %in% tolower(tableNames))
-  DatabaseConnector::renderTranslateExecuteSql(connection, "DROP TABLE @database_schema.dqd_results;", database_schema = resultsDatabaseSchemaEunomia)
 })
 
 test_that("Execute DQ checks and write to table", {
@@ -246,33 +201,6 @@ test_that("Execute DQ checks and write to table", {
   tableNames <- DatabaseConnector::getTableNames(connection = connection, databaseSchema = resultsDatabaseSchemaEunomia)
   expect_true("dqd_results" %in% tolower(tableNames))
   DatabaseConnector::renderTranslateExecuteSql(connection, "DROP TABLE @database_schema.dqd_results;", database_schema = resultsDatabaseSchemaEunomia)
-})
-
-test_that("Execute reEvaluateThresholds on Synthea/Eunomia", {
-  outputFolder <- tempfile("dqd_")
-  on.exit(unlink(outputFolder, recursive = TRUE))
-
-  expect_warning(
-    results <- executeDqChecks(
-      connectionDetails = connectionDetailsEunomia,
-      cdmDatabaseSchema = cdmDatabaseSchemaEunomia,
-      resultsDatabaseSchema = resultsDatabaseSchemaEunomia,
-      cdmSourceName = "Eunomia",
-      checkNames = "measurePersonCompleteness",
-      outputFolder = outputFolder,
-      writeToTable = F
-    ),
-    regexp = "^Missing check names.*"
-  )
-
-  jsonPath <- list.files(outputFolder, ".json", full.names = TRUE)
-
-  results2 <- reEvaluateThresholds(
-    jsonFilePath = jsonPath,
-    outputFolder = outputFolder,
-    outputFile = "reEvaluated.txt"
-  )
-  expect_type(results2, "list")
 })
 
 test_that("Execute DQ checks using sqlOnly=TRUE and sqlOnlyUnionCount=4 and sqlOnlyIncrementalInsert=TRUE", {
