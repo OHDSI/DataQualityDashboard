c3d375a7a507bc012ceb02c956df4ba31a62bd06
Katy Sadowski <katy.h.sadowski@gmail.com>
2023-11-03 10:48:46 -0400

    forgot comma

--- R/executeDqChecks.R
+++ R/executeDqChecks.R
@@ -139,7 +139,7 @@ executeDqChecks <- function(connectionDetails,
       stop("Please populate the cdm_source table before executing data quality checks.")
     }
     if (nrow(metadata) > 1) {
-      metadata <- metadata[1]
+      metadata <- metadata[1,]
       warning("The cdm_source table has more than 1 row. A single row from this table has been selected to populate DQD metadata.")
     }
     metadata$dqdVersion <- as.character(packageVersion("DataQualityDashboard"))
