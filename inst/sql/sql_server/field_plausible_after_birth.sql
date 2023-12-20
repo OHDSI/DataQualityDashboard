4c98f19dbc0bc0b46954912d083dd5338a949d11
Maxim Moinat <maximmoinat@gmail.com>
2023-12-20 15:42:04 +0100

    add sql for new temporal plausibility checks

---
+++ inst/sql/sql_server/field_plausible_after_birth.sql
@@ -0,0 +1,62 @@
+/*********
+PLAUSIBLE_AFTER_BIRTH
+all events happen after birth (PLAUSIBLE_AFTER_BIRTH == Yes)
+Birthdate is either birth_datetime or composed from year_of_birth, month_of_birth, day_of_birth (taking 1st month/1st day if missing).
+Denominator is number of events.
+
+Parameters used in this template:
+cdmDatabaseSchema = @cdmDatabaseSchema
+cdmTableName = @cdmTableName
+cdmFieldName = @cdmFieldName
+{@cohort & '@runForCohort' == 'Yes'}?{
+cohortDefinitionId = @cohortDefinitionId
+cohortDatabaseSchema = @cohortDatabaseSchema
+cohortTableName = @cohortTableName
+}
+**********/
+
+
+SELECT 
+    num_violated_rows, 
+    CASE 
+        WHEN denominator.num_rows = 0 THEN 0 
+        ELSE 1.0*num_violated_rows/denominator.num_rows 
+    END AS pct_violated_rows, 
+    denominator.num_rows AS num_denominator_rows
+FROM
+(
+    SELECT 
+        COUNT_BIG(violated_rows.violating_field) AS num_violated_rows
+    FROM
+    (
+        /*violatedRowsBegin*/
+        SELECT 
+            '@cdmTableName.@cdmFieldName' AS violating_field, 
+            cdmTable.*
+        FROM @cdmDatabaseSchema.@cdmTableName cdmTable
+        {@cohort & '@runForCohort' == 'Yes'} ? {
+        JOIN @cohortDatabaseSchema.@cohortTableName c ON cdmTable.person_id = c.subject_id
+            AND c.COHORT_DEFINITION_ID = @cohortDefinitionId
+        }
+        JOIN @cdmDatabaseSchema.person p ON cdmTable.person_id = de.person_id
+        WHERE CAST(cdmTable.@cdmFieldName AS DATE) < COALESCE(
+                p.birth_datetime, 
+                CAST(CONCAT(
+                    p.year_of_birth, '-',
+                    COALESCE(p.month_of_birth, 1), '-',
+                    COALESCE(p.day_of_birth, 1)
+                ) AS DATE)
+            )
+        /*violatedRowsEnd*/
+    ) violated_rows
+) violated_row_count,
+(
+    SELECT 
+        COUNT_BIG(*) AS num_rows
+    FROM @cdmDatabaseSchema.@cdmTableName cdmTable
+    {@cohort & '@runForCohort' == 'Yes'} ? {
+    JOIN @cohortDatabaseSchema.@cohortTableName c ON cdmTable.person_id = c.subject_id
+        AND c.cohort_definition_id = @cohortDefinitionId
+    }
+) denominator
+;
---
+++ inst/sql/sql_server/field_plausible_start_before_end.sql
@@ -0,0 +1,54 @@
+
+/*********
+PLAUSIBLE_START_BEFORE_END
+all start dates are before their corresponding end dates (PLAUSIBLE_START_BEFORE_END == Yes)
+
+Parameters used in this template:
+schema = @schema
+cdmTableName = @cdmTableName
+cdmFieldName = @cdmFieldName
+plausibleTemporalAfterTableName = @plausibleTemporalAfterTableName
+plausibleTemporalAfterFieldName = @plausibleTemporalAfterFieldName
+{@cohort & '@runForCohort' == 'Yes'}?{
+cohortDefinitionId = @cohortDefinitionId
+cohortDatabaseSchema = @cohortDatabaseSchema
+cohortTableName = @cohortTableName
+}
+**********/
+
+SELECT 
+    num_violated_rows, 
+    CASE 
+        WHEN denominator.num_rows = 0 THEN 0 
+        ELSE 1.0*num_violated_rows/denominator.num_rows 
+    END AS pct_violated_rows, 
+    denominator.num_rows AS num_denominator_rows
+FROM
+(
+    SELECT 
+        COUNT_BIG(violated_rows.violating_field) AS num_violated_rows
+    FROM
+    (
+        /*violatedRowsBegin*/
+        SELECT 
+            '@cdmTableName.@cdmFieldName' AS violating_field, 
+            cdmTable.*
+        FROM @schema.@cdmTableName cdmTable
+        {@cohort & '@runForCohort' == 'Yes'} ? {
+        JOIN @cohortDatabaseSchema.@cohortTableName c ON cdmTable.person_id = c.subject_id
+            AND c.cohort_definition_id = @cohortDefinitionId
+        }
+        WHERE CAST(cdmTable.@cdmFieldName AS DATE) < CAST(cdmTable.@plausibleTemporalAfterFieldName AS DATE)
+        /*violatedRowsEnd*/
+    ) violated_rows
+) violated_row_count,
+(
+    SELECT 
+        COUNT_BIG(*) AS num_rows
+    FROM @schema.@cdmTableName cdmTable
+    {@cohort & '@runForCohort' == 'Yes'} ? {
+    JOIN @cohortDatabaseSchema.@cohortTableName c ON cdmTable.person_id = c.subject_id
+        AND c.cohort_definition_id = @cohortDefinitionId 
+    }
+) denominator
+;
