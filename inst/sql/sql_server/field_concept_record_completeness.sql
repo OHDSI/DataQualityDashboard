a8272ffd9578d925fc454ff41ed5215c585f12a6
Katy Sadowski <katy.sadowski@gmail.com>
2025-08-04 19:58:07 -0400

    fix numerator

--- inst/sql/sql_server/field_concept_record_completeness.sql
+++ inst/sql/sql_server/field_concept_record_completeness.sql
@@ -36,7 +36,26 @@ FROM (
                 AND c.cohort_definition_id = @cohortDefinitionId
         }
         WHERE cdmTable.@cdmFieldName = 0
-        {@isRequired == 'No'}?{OR cdmTable.@cdmFieldName IS NULL}
+        {@cdmFieldName == 'UNIT_CONCEPT_ID' | @cdmFieldName == 'UNIT_SOURCE_CONCEPT_ID'}?{OR (cdmTable.@cdmFieldName IS NULL AND cdmTable.unit_source_value IS NOT NULL)}
+        {@cdmFieldName == 'ADMITTED_FROM_CONCEPT_ID'}?{OR (cdmTable.@cdmFieldName IS NULL AND cdmTable.admitted_from_source_value IS NOT NULL)}
+        {@cdmFieldName == 'ADMITTING_SOURCE_CONCEPT_ID'}?{OR (cdmTable.@cdmFieldName IS NULL AND cdmTable.admitting_source_value IS NOT NULL)}
+        {@cdmFieldName == 'DISCHARGED_TO_CONCEPT_ID'}?{OR (cdmTable.@cdmFieldName IS NULL AND cdmTable.discharged_to_source_value IS NOT NULL)}
+        {@cdmFieldName == 'DISCHARGE_TO_CONCEPT_ID'}?{OR (cdmTable.@cdmFieldName IS NULL AND cdmTable.discharge_to_source_value IS NOT NULL)}
+        {@cdmFieldName == 'CONDITION_STATUS_CONCEPT_ID'}?{OR (cdmTable.@cdmFieldName IS NULL AND cdmTable.condition_status_source_value IS NOT NULL)}
+        {@cdmFieldName == 'ROUTE_CONCEPT_ID'}?{OR (cdmTable.@cdmFieldName IS NULL AND cdmTable.route_source_value IS NOT NULL)}
+        {@cdmFieldName == 'MODIFIER_CONCEPT_ID'}?{OR (cdmTable.@cdmFieldName IS NULL AND cdmTable.modifier_source_value IS NOT NULL)}
+        {@cdmFieldName == 'QUALIFIER_CONCEPT_ID'}?{OR (cdmTable.@cdmFieldName IS NULL AND cdmTable.qualifier_source_value IS NOT NULL)}
+        {@cdmFieldName == 'CAUSE_CONCEPT_ID' | @cdmFieldName == 'CAUSE_SOURCE_CONCEPT_ID'}?{OR (cdmTable.@cdmFieldName IS NULL AND cdmTable.cause_source_value IS NOT NULL)}
+        {@cdmFieldName == 'ANATOMIC_SITE_CONCEPT_ID'}?{OR (cdmTable.@cdmFieldName IS NULL AND cdmTable.anatomic_site_source_value IS NOT NULL)}
+        {@cdmFieldName == 'DISEASE_STATUS_CONCEPT_ID'}?{OR (cdmTable.@cdmFieldName IS NULL AND cdmTable.disease_status_source_value IS NOT NULL)}
+        {@cdmFieldName == 'COUNTRY_CONCEPT_ID'}?{OR (cdmTable.@cdmFieldName IS NULL AND cdmTable.country_source_value IS NOT NULL)}
+        {@cdmFieldName == 'PLACE_OF_SERVICE_CONCEPT_ID'}?{OR (cdmTable.@cdmFieldName IS NULL AND cdmTable.place_of_service_source_value IS NOT NULL)}
+        {@cdmFieldName == 'SPECIALTY_CONCEPT_ID' | @cdmFieldName == 'SPECIALTY_SOURCE_CONCEPT_ID'}?{OR (cdmTable.@cdmFieldName IS NULL AND cdmTable.specialty_source_value IS NOT NULL)}
+        {@cdmTableName == 'PROVIDER' & (@cdmFieldName == 'GENDER_CONCEPT_ID' | @cdmFieldName == 'GENDER_SOURCE_CONCEPT_ID')}?{OR (cdmTable.@cdmFieldName IS NULL AND cdmTable.gender_source_value IS NOT NULL)}
+        {@cdmFieldName == 'PAYER_CONCEPT_ID' | @cdmFieldName == 'PAYER_SOURCE_CONCEPT_ID'}?{OR (cdmTable.@cdmFieldName IS NULL AND cdmTable.payer_source_value IS NOT NULL)}
+        {@cdmFieldName == 'PLAN_CONCEPT_ID' | @cdmFieldName == 'PLAN_SOURCE_CONCEPT_ID'}?{OR (cdmTable.@cdmFieldName IS NULL AND cdmTable.plan_source_value IS NOT NULL)}
+        {@cdmFieldName == 'SPONSOR_CONCEPT_ID' | @cdmFieldName == 'SPONSOR_SOURCE_CONCEPT_ID'}?{OR (cdmTable.@cdmFieldName IS NULL AND cdmTable.sponsor_source_value IS NOT NULL)}
+        {@cdmFieldName == 'STOP_REASON_CONCEPT_ID' | @cdmFieldName == 'STOP_REASON_SOURCE_CONCEPT_ID'}?{OR (cdmTable.@cdmFieldName IS NULL AND cdmTable.stop_reason_source_value IS NOT NULL)}
         /*violatedRowsEnd*/
     ) violated_rows
 ) violated_row_count,
