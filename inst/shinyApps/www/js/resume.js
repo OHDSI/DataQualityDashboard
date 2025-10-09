b40aa7f76651ab0cff86a90725b0ee69c8b53201
Maxim Moinat <maximmoinat@gmail.com>
2025-10-09 16:15:44 -0400

    delete vender js and css

--- inst/shinyApps/www/js/resume.js
+++ inst/shinyApps/www/js/resume.js
@@ -15,14 +15,4 @@
     }
   });
 
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
-
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#sideNav'
-  });
-
 })(jQuery); // End of use strict
